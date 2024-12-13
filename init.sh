5aee906ac312601298bb86754fe28e82bbf54308
Michael Matloka <michael@matloka.com>
2024-12-13 01:30:00 +0100

    Fix `fisher` setup

--- init.sh
+++ init.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+# Put config files in place
 mkdir -p ~/.config
 rsync -avh --no-perms ./.config/ ~/.config/
+
+# Run OS-specific init
 if [[ $(uname) == 'Darwin' ]]; then
     ./init-macos.sh
 fi
+
+# Mark fish as a valid shell
+echo '/opt/homebrew/bin/fish' | sudo tee -a /etc/shells
+
+# Switch to fish as the default shell
+sudo chsh -s /opt/homebrew/bin/fish $USER
+
+# Install Fisher
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
+
+# Install Fisher packages
+fish -c 'fisher update && fisher install jorgebucaran/nvm.fish'
