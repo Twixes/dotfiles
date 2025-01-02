9b5b91a718e9a4d985573666111403af90163f14
Michael Matloka <michael@matloka.com>
2025-01-03 00:30:12 +0100

    Add git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,29 @@ pyenv virtualenv-init - | source
 
 status --is-interactive
 and starship init fish | source
+
+# git aliases
+
+alias g='git'
+
+alias ga='git add'
+alias gaa='git add --all'
+
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias grc='git rebase --continue'
+alias gri='git rebase --interactive'
+alias grm='git rebase master'
+alias grim='git rebase --interactive master'
+alias grom='git rebase --onto master'
+
+alias gm='git merge'
+alias gma='git merge --abort'
+alias gmc='git merge --continue'
+alias gmi='git merge --interactive'
+alias gmm='git merge master'
+alias gmmi='git merge master --interactive'
+
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+alias gpf!='git push --force'
